f8a0949f372152ea0e333e109be7ca4e506cfaa4
eliabruni <elia.bruni@gmail.com>
2017-03-09 16:05:40 +0100

    adding rmsprop for wasser

--- OpenNMT/onmt/Constants.py
+++ OpenNMT/onmt/Constants.py
@@ -1,12 +1,23 @@
 
-PAD = 100001
-UNK = 100002
-BOS = 100003
-EOS = 100004
-IEOS = 100005
+PAD = 0
+UNK = 1
+BOS = 2
+EOS = 3
 
 PAD_WORD = '<blank>'
 UNK_WORD = '<unk>'
 BOS_WORD = '<s>'
 EOS_WORD = '</s>'
-IEOS_WORD = '</ieos>'
+
+
+# PAD = 100001
+# UNK = 100002
+# BOS = 100003
+# EOS = 100004
+# IEOS = 100005
+#
+# PAD_WORD = '<blank>'
+# UNK_WORD = '<unk>'
+# BOS_WORD = '<s>'
+# EOS_WORD = '</s>'
+# IEOS_WORD = '</ieos>'
