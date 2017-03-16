94b75d018b8bf50ece5159e01b132ba0ae1aefe5
eliabruni <elia.bruni@gmail.com>
2017-03-16 18:14:52 +0100

    temp print fix

--- OpenNMT/gan_train.py
+++ OpenNMT/gan_train.py
@@ -441,7 +441,7 @@ def trainModel(G, trainData, validData, dataset, optimizerG, D=None, optimizerD=
                 if opt.hallucinate:
                     total_loss, report_loss = 0, 0
                     total_words, report_words = 0, 0
-                    for j in range(20):
+                    for j in range(10):
                         H1.zero_grad()
                         h_outputs = H1(batch)
                         sources = batch[0]
@@ -474,7 +474,7 @@ def trainModel(G, trainData, validData, dataset, optimizerG, D=None, optimizerD=
 
                     total_loss, report_loss = 0, 0
                     total_words, report_words = 0, 0
-                    for j in range(20):
+                    for j in range(10):
                         H2.zero_grad()
                         inverse_sources = batch[1][1:-1]
                         inverse_targets = batch[0]
