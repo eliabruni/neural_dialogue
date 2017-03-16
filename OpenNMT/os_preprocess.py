e3a7cd5c0bd2c1700d52ea4ce1599add0328dedf
eliabruni <elia.bruni@gmail.com>
2017-03-16 22:29:56 +0100

    temp print fix

--- OpenNMT/os_preprocess.py
+++ OpenNMT/os_preprocess.py
@@ -79,7 +79,7 @@ def makeOSdata(srcFile):
     while True:
         lines = srcF.readline().split('|')
 
-        if count == 10000000:
+        if count >= 10000000:
             print('lines: ' + str(lines))
         if not lines:
             break
