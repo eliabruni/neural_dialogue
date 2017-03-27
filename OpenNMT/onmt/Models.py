b53b1ca023036b542f102f0de2497eb6402b3f35
eliabruni <elia.bruni@gmail.com>
2017-03-27 20:55:30 +0200

    temp print fix

--- OpenNMT/onmt/Models.py
+++ OpenNMT/onmt/Models.py
@@ -436,7 +436,11 @@ class CNN(nn.Module):
 
         self.embed = nn.Linear(V, D)
         print('Ci, Co, (K, D): ' + str(Ci) + ' '+  str(Co) + ' '  + str(D))
-        self.convs1 = [nn.Conv2d(Ci, Co, (K, D)) for K in Ks]
+
+        if self.opt.cuda:
+            self.convs1 = [nn.Conv2d(Ci, Co, (K, D)).cuda() for K in Ks]
+        else:
+            self.convs1 = [nn.Conv2d(Ci, Co, (K, D)) for K in Ks]
         '''
         self.conv13 = nn.Conv2d(Ci, Co, (3, D))
         self.conv14 = nn.Conv2d(Ci, Co, (4, D))
@@ -460,6 +464,9 @@ class CNN(nn.Module):
             x = Variable(x)
 
         x = x.unsqueeze(1)  # (N,Ci,W,D)
+
+        if self.opt.cuda:
+            x = x.cuda()
 
         x = [F.relu(conv(x)).squeeze(3) for conv in self.convs1]  # [(N,Co,W), ...]*len(Ks)
         x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]  # [(N,Co), ...]*len(Ks)
